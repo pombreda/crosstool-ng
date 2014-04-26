3c8b595104260063ed0e4d92dc0897f5be2da393
Ray Donnelly <mingw.android@gmail.com>
2014-04-26 14:15:21 +0100

    Add gettext and libiconv as a companion lib
    .. it's needed for the RPC stuff in {e}glibc.
    
    Building both is avoided on Linux and building
    iconv is avoided on Darwin.

--- scripts/build/libc/glibc.sh
+++ scripts/build/libc/glibc.sh
@@ -375,6 +375,21 @@ do_libc_backend_once() {
     CT_LDFLAGS_FOR_BUILD+=" ${CT_EXTRA_LDFLAGS_FOR_BUILD}"
     extra_make_args+=( "BUILD_CFLAGS=${CT_CFLAGS_FOR_BUILD}" "BUILD_LDFLAGS=${CT_LDFLAGS_FOR_BUILD}" )
 
+    case "$CT_BUILD" in
+        *mingw*)
+            # When installing headers on MinGW-w64, sunrpc needs gettext for building
+            # cross-rpcgen.
+            # used to parse the output of the C-preprocessor.
+            extra_make_args+=( BUILD_CPPFLAGS="-I${CT_BUILDTOOLS_PREFIX_DIR}/include/" )
+            extra_make_args+=( BUILD_LDFLAGS="-L${CT_BUILDTOOLS_PREFIX_DIR}/lib -Wl,-Bstatic -lintl -Wl,-Bdynamic" )
+            ;;
+        *darwin*)
+            # .. and the same goes for Darwin.
+            extra_make_args+=( BUILD_CPPFLAGS="-I${CT_BUILDTOOLS_PREFIX_DIR}/include/" )
+            extra_make_args+=( BUILD_LDFLAGS="-L${CT_BUILDTOOLS_PREFIX_DIR}/lib -lintl" )
+            ;;
+    esac
+
     if [ "${libc_headers}" = "y" ]; then
         CT_DoLog EXTRA "Installing C library headers"
 
